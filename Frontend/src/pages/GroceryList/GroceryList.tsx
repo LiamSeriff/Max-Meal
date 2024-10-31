038c97933d032e9be687f9de0e044d0ffebc2e01
LiamSeriff <liamseriff@gmail.com>
2024-10-31 17:41:58 -0400

    Started working on GroceryList page

--- Frontend/src/pages/GroceryList/GroceryList.tsx
+++ Frontend/src/pages/GroceryList/GroceryList.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { PageContainer } from "./GroceryList_Data";
+import {
+  PageContainer,
+  IngredientsContainer,
+  HeadingContainer,
+  Heading,
+  MyGroceryListContainer,
+  InputContainer,
+  Input,
+} from "./GroceryList_Data";
 
 const GroceryList = () => {
   return (
     <>
       <PageContainer>
-        <div>Grocery List</div>
+        <HeadingContainer>
+          <Heading>GROCERY LIST</Heading>
+        </HeadingContainer>
+        <MyGroceryListContainer>b</MyGroceryListContainer>
+        <IngredientsContainer>
+          <InputContainer>
+            <Input
+              type="search"
+              className="form-control"
+              id="ingredients search"
+              placeholder="Search ingredients"
+            ></Input>
+          </InputContainer>
+        </IngredientsContainer>
       </PageContainer>
     </>
   );
--- Frontend/src/pages/GroceryList/GroceryList_Data.tsx
+++ Frontend/src/pages/GroceryList/GroceryList_Data.tsx
@@ -1,7 +1,63 @@
 import styled from "styled-components";
 
 export const PageContainer = styled.div`
+  position: relative;
   height: 100vh;
   width: 100vw;
   background-color: tan;
+`;
+
+export const IngredientsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 15vh;
+  left: 5vw;
+  height: 70vh;
+  width: 30vw;
+  border: black;
+  border-style: solid;
+`;
+
+export const HeadingContainer = styled.div`
+  position: absolute;
+  top: 3vh;
+  left: 5vw;
+  height: 10vh;
+  width: 30vw;
+  border: black;
+  border-style: solid;
+  display: flex;
+  justify-content: center;
+`;
+
+export const MyGroceryListContainer = styled.div`
+  position: absolute;
+  top: 3vh;
+  left: 45vw;
+  height: 82vh;
+  width: 45vw;
+  border: black;
+  border-style: solid;
+`;
+
+export const InputContainer = styled.div`
+  position: absolute;
+  top: 2%;
+  left: 2%;
+  height: 5%;
+  width: 96%;
+`;
+
+export const Input = styled.input`
+  border-color: black;
+  border-width: 3px;
+  border-style: solid;
+`;
+
+export const Heading = styled.h1`
+  position: absolute;
+  top: 25%;
+  font-size: 3rem;
+  color: black;
 `;
